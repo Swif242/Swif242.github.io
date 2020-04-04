f840e023165f2083dee6b3bb554ed3a03480094b
Jeremy N <no-reply>
2020-04-03 18:15:34 -0700

    added to and edited contact page, added quote to footer, added current date with moment.js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// contact page variables
+const contact = document.getElementById("contact");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+const resume = document.getElementById("resume");
+
+// current date
+const date = moment().format("MMM-DD-YYYY");
+console.log(date);
+$("#date").text("Today is " + date);
+
+
+
+
+$(phone).on("click", function(){
+    phone.textContent ="520-302-6653";
+    console.log("test")
+});
+
+$(email).on("click", function(){
+    email.textContent = "JRNels242@gmail.com";
+    console.log("test")
+});
+
